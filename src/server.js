f52d294776a3e31f7bdf69d599fd3bf9d3ced4c7
Halyna <galacuvaeva4@gmail.com>
2026-05-10 20:00:35 +0300

    finish task

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { ErrorHandler } from './middleware/errorHandler.js';
-
+import notesRoutes from './routes/notesRoutes.js';
 const PORT = process.env.PORT ?? 3000;
 const app = express();
 
@@ -13,7 +13,7 @@ app.use(logger);
 app.use(express.json());
 app.use(cors());
 
-
+app.use(notesRoutes);
 
 app.use(notFoundHandler);
 
