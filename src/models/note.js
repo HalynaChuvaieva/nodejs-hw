b4f2d1da7f8125e918ee3c216f405839d442e99f
Halyna <galacuvaeva4@gmail.com>
2026-05-17 19:29:37 +0300

    fix index tag

--- src/models/note.js
+++ src/models/note.js
@@ -27,6 +27,8 @@ const noteSchema = new Schema(
  },
 );
 
+noteSchema.index({ title: 'text', content: 'text'});
+noteSchema.index({ tag: 1 });
+
 export const Note = model('Note', noteSchema);
 
-noteSchema.index({ title: 'text', content: 'text', tag: 'text'});
