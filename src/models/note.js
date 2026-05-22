ffc171f84382e1588966b8bf01af864f6e36b9be
Halyna <galacuvaeva4@gmail.com>
2026-05-22 16:26:10 +0300

    add authentication

--- src/models/note.js
+++ src/models/note.js
@@ -19,7 +19,12 @@ const noteSchema = new Schema(
    required: false,
    enum: TAGS,
    default: 'Todo',
-  },
+    },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  }
  },
  {
   timestamps: true,
@@ -27,7 +32,7 @@ const noteSchema = new Schema(
  },
 );
 
-noteSchema.index({ title: 'text', content: 'text'});
+noteSchema.index({ userId: 1, title: 'text', content: 'text' });
 noteSchema.index({ tag: 1 });
 
 export const Note = model('Note', noteSchema);
--- src/server.js
+++ src/server.js
@@ -6,14 +6,19 @@ import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
+
 const PORT = process.env.PORT ?? 3000;
 const app = express();
 
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
+app.use(authRoutes);
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
